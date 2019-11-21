2e002400e357601501acf427a3b2a4fe17617fb1
rankhep <ehdrms1507@naver.com>
2019-11-21 18:56:24 +0900

    Add user model

---
+++ app/src/main/java/com/example/realworldtutorial/model/User.kt
@@ -0,0 +1,16 @@
+package com.example.realworldtutorial.model
+
+class User(
+    var email: String,
+    var token: String,
+    var username: String,
+    var bio: String,
+    var image: String = ""
+)
+//{
+//    "email": "jake@jake.jake",
+//    "token": "jwt.token.here",
+//    "username": "jake",
+//    "bio": "I work at statefarm",
+//    "image": null
+//}
---
+++ app/src/main/java/com/example/realworldtutorial/model/UserBox.kt
@@ -0,0 +1,3 @@
+package com.example.realworldtutorial.model
+
+class UserBox(var user: User)
