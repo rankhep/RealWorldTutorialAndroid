372bc849cbbb4e3ca21c1ca815882e9a42692b3a
rankhep <ehdrms1507@naver.com>
2019-11-21 18:53:24 +0900

    Add NetworkHelper

---
+++ app/src/main/java/com/example/realworldtutorial/util/NetworkHelper.kt
@@ -0,0 +1,21 @@
+package com.example.realworldtutorial.util
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkHelper {
+    companion object {
+        var retrofit: Retrofit? = null
+        val baseUrl = ""
+        val networkInstance: RetrofitInterface
+            get() {
+                retrofit?.run {
+                    retrofit = Retrofit.Builder()
+                        .baseUrl(baseUrl())
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build()
+                }
+                return retrofit!!.create(RetrofitInterface::class.java)
+            }
+    }
+}
---
+++ app/src/main/java/com/example/realworldtutorial/util/RetrofitInterface.kt
@@ -0,0 +1,5 @@
+package com.example.realworldtutorial.util
+
+interface RetrofitInterface {
+
+}
